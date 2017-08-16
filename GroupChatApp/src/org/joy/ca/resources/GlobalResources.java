4b9466eca8784ba930f61e4e8ce70c53d4a84477
Joydeep Dey <dey.joydeep.cse@gmail.com>
2017-08-16 12:20:15 +0900

    Code added

--- GroupChatApp/src/org/joy/ca/resources/GlobalResources.java
+++ GroupChatApp/src/org/joy/ca/resources/GlobalResources.java
@@ -1,4 +1,24 @@
 package org.joy.ca.resources;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.joy.ca.beans.MessageBean;
+import org.joy.ca.beans.UserInfo;
+
 public class GlobalResources {
+	/********************************************************************************************/
+	// Below two fields are required to revise for their usage
+	public static long msgId = 0;
+	public static final Map<Long, MessageBean> MSG_MAP = new TreeMap<>();
+	/********************************************************************************************/
+
+	// TODO: DB replacement
+	public static final List<UserInfo> USER_LIST = new ArrayList<>();
+
+	// TODO: to be handled as noted in InitServlet.init()
+	public static final Map<String, String> RESOURCE_PATH_MAP = new HashMap<>();
 }
