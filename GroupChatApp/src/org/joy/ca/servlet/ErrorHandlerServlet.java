e90e80541e9fac7db81367d8d5f92ea0cf58cf9e
Joydeep Dey <dey.joydeep.cse@gmail.com>
2017-08-16 12:24:49 +0900

    Code added

--- GroupChatApp/src/org/joy/ca/servlet/ErrorHandlerServlet.java
+++ GroupChatApp/src/org/joy/ca/servlet/ErrorHandlerServlet.java
@@ -1,5 +1,30 @@
 package org.joy.ca.servlet;
 
-public class ErrorHandlerServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.joy.ca.resources.CommonResources;
+
+/**
+ * Handle redirection to error page on occurring of either exception or HTTP
+ * error. The error redirection is directly being controlled from web.xml
+ *
+ * @author Joydeep Dey
+ *
+ */
+@WebServlet("/error")
+public class ErrorHandlerServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.getRequestDispatcher(CommonResources.ERROR_PAGE).forward(req, resp);
+	}
 }
