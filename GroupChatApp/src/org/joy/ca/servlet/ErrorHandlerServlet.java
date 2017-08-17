2b3559fdd69cffb44b3d7f72021c65e924072d94
Joydeep Dey <dey.joydeep.cse@gmail.com>
2017-08-17 16:57:18 +0900

    method content modified
    doPost() added and doGet() is modified to call doPost()

--- GroupChatApp/src/org/joy/ca/servlet/ErrorHandlerServlet.java
+++ GroupChatApp/src/org/joy/ca/servlet/ErrorHandlerServlet.java
@@ -25,6 +25,12 @@ public class ErrorHandlerServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		req.getRequestDispatcher(CommonResources.ERROR_PAGE).forward(req, resp);
 	}
 }
